0714246ec07c3df777b233d29d241b2dda42cbe2
LUCI031 <wlsrb031@naver.com>
2022-04-06 22:04:42 +0900

    060422 TIL Python

--- Python/baekjoon/baekjoon.py
+++ Python/baekjoon/baekjoon.py
@@ -1,2 +1,19 @@
-A, B = int(input('A : ')), int(input('B : '))
-print(A+B)
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+
+result = 0
+for i in range(n):
+    word = input()
+    buf = {}
+    result += 1
+    for j in word:
+        if j in buf.keys():
+            if prev == j:
+                continue
+            result -= 1
+            break
+        buf[j] = True
+        prev = j
+print(result)
---
+++ Python/numpy/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(np.dot(m1, m2))
