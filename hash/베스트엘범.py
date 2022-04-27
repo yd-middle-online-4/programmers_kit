091776bd925c8ace07286d2c15328302e11a514f
Hyeonkyu Lee <leehyeonkyu@naver.com>
2022-04-28 01:06:53 +0900

    .

--- hash/베스트엘범.py
+++ hash/베스트엘범.py
@@ -20,11 +20,54 @@ def solution(genres, plays):
         
     play_t = sorted(play_t.items(), key = lambda x: x[1], reverse = True)
     
+    print(play_t)
+    print(order_g)
 
     for i in play_t:
         order_g[i[0]] = sorted(order_g[i[0]], key = lambda x: x[0], reverse = True)[:2]
         for (x,y) in order_g[i[0]]:
             answer.append(y)
-
     print(answer)
 solution(genres, plays)
+
+
+'''
+처음 시도했던 방법 answer에 추가하는 부분에서 입출력 예시 였던 genres의 리스트 길이가 길어지면 list index out of range 가 나서 문제였던것.
+def solution(genres, plays):
+    answer = []
+    play_time = {} # 총 플레이 수
+    order_g = {} # 장르별 순서와 플레이 수
+
+    for i in range(len(genres)):
+        if genres[i] in play_time.keys():
+            play_time[genres[i]]+=plays[i]
+            order_g[genres[i]].append((plays[i],i))   
+        else:
+            play_time[genres[i]] = plays[i]
+            order_g[genres[i]] = [(plays[i],i)]
+           
+    play_time = sorted(play_time.items(), key = lambda x: x[1], reverse = True)
+    
+    #  문제였던 부분
+    # for i in play_time:
+    #     order_g[i[0]] = sorted(order_g[i[0]], key = lambda x: x[0], reverse = True)
+    #     for j in range(len(order_g)):
+    #         if j == 2:
+    #             break
+    #         answer.append(order_g[i[0]][j][1])
+
+    #고친부분 
+    for i in play_time:
+        order_g[i[0]] = sorted(order_g[i[0]], key = lambda x: x[0], reverse = True)[:2]
+        for (x,y) in order_g[i[0]]:
+            answer.append(y)
+  
+    return(answer)
+    
+    
+genres = ["classic", "pop", "classic", "classic", "pop", "k-pop"]
+plays = [500, 600, 150, 800, 2500, 4000]
+
+print(solution(genres, plays))
+
+'''
